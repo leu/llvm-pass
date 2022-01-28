345c137c3f47903a533d878b01d05ee8922abf20
Daniel <daniel.simols@outlook.com>
2022-01-28 23:00:14 +0000

    revert program.c back to original

--- program.c
+++ program.c
@@ -15,24 +15,3 @@ int main(int argc, char** argv) {
   
   return 0;
 }
-
-int hello(int argc, char** argv) {
-  if (argc != 2) {
-    printf("The program expects one argument\n");
-    return 1;
-  }
-
-  if (argc != 2) {
-    printf("The program expects one argument\n");
-    return 1;
-  }
-  
-  if (argv[1][0] == 'A')
-    printf("A\n");
-  else 
-    if (argv[1][0] == 'B')
-      printf("B\n");
-    else printf("C\n");
-  
-  return 0;
-}
